c22fb6a4fad3bbbc3ad9be6aa8fe293d329e8484
Herbert Parasio <91493748+Herbertph@users.noreply.github.com>
2025-05-12 13:26:13 -0300

    Project Setup
    Vite + Vue -> Done
    Tailwind -> Done
    Router -> Done

---
+++ recipe-app/src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '@/pages/HomePage.vue'
+import RecipePage from '@/pages/RecipePage.vue'
+
+const routes = [
+  { path: '/', component: HomePage },
+  { path: '/recipe/:id', component: RecipePage },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
