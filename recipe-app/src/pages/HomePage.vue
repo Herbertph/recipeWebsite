cd4e5becb4b8518e607789b8ff741160644ab63a
Herbert Parasio <91493748+Herbertph@users.noreply.github.com>
2025-05-13 13:18:42 -0300

    Little update on H2

--- recipe-app/src/pages/HomePage.vue
+++ recipe-app/src/pages/HomePage.vue
@@ -6,7 +6,7 @@
       class="bg-[#FAF4EF] min-h-screen flex flex-col justify-center items-center gap-8 px-6 py-16">
 
       <!-- Section Heading -->
-      <h2 class="text-2xl sm:text-3xl font-semibold text-[#3B2F2F] tracking-wide">
+      <h2 class="text-2xl sm:text-3xl font-bold text-[#3B2F2F] leading-snug tracking-wide">
         Choose your delicious recipe
       </h2>
 
