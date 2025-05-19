429b769bae6bf032aa26fd868a51da14da190b7b
Herbert Parasio <91493748+Herbertph@users.noreply.github.com>
2025-05-18 21:23:22 -0300

    Api.js Refactored and Catching errors

--- recipe-app/src/services/api.js
+++ recipe-app/src/services/api.js
@@ -1,30 +1,40 @@
 // src/services/api.js
 
 const BASE_URL = 'https://api.spoonacular.com/recipes'
+const API_KEY=import.meta.env.VITE_SPOONACULAR_API_KEY2
 
-export async function searchRecipes(query, cuisine, offset = 0, number = 5) {
-  const params = new URLSearchParams({
-    apiKey: import.meta.env.VITE_SPOONACULAR_API_KEY2,
-    query,
-    cuisine,
-    offset,
-    number,
-    addRecipeInformation: true
-  })
-
-  const response = await fetch(`${BASE_URL}/complexSearch?${params}`)
-  if (!response.ok) {
-    throw new Error('Failed to fetch recipes')
-  }
-
+async function fetchJson(url){
+  const response = await fetch(url)
+  if (!response.ok) throw new Error('Failed to fetch data')
   return await response.json()
 }
 
+export async function searchRecipes(query, cuisine, offset = 0, number = 5) {
+  try {
+    const params = new URLSearchParams({
+      apiKey: API_KEY,
+      query,
+      cuisine,
+      offset,
+      number,
+      addRecipeInformation: true
+    })
+
+    const url = `${BASE_URL}/complexSearch?${params}`
+    return await fetchJson(url)
+  } catch (error) {
+    console.error('Error searching recipes:', error)
+    throw error
+  }
+}
+
 export async function getRecipeInformation(id) {
-  const response = await fetch(
-    `${BASE_URL}/${id}/information?apiKey=${import.meta.env.VITE_SPOONACULAR_API_KEY2}`
-  )
-  if (!response.ok) throw new Error('Failed to fetch recipe')
-  return await response.json()
+  try {
+    const url = `${BASE_URL}/${id}/information?apiKey=${API_KEY}`
+    return await fetchJson(url)
+  } catch (error) {
+    console.error('Error fetching recipe information:', error)
+    throw error
+  }
 }
 
